f86b81976990b2bd5ca881ebbeb54cdd2b9eb627
Daniel-Sameh <111789605+Daniel-Sameh@users.noreply.github.com>
2026-03-28 23:04:35 +0200

    Initial commit: Testing Assignment

---
+++ Part 1/java-math-library-master/src/App.java
@@ -0,0 +1,18 @@
+import de.tilman_neumann.util.Multiset;
+import de.tilman_neumann.util.Multiset_HashMapImpl;
+import de.tilman_neumann.util.Pair;
+
+public class App {
+    public static void main(String[] args) {
+        Pair<String, Integer> p = new Pair<String, Integer>("hassan", 10);
+        Multiset<String> m = new Multiset_HashMapImpl<String>();
+        int x = m.add("hassan");
+        System.out.println(x);
+        int y =m.add("hassan", -3);
+        System.out.println(y);
+        x = m.add("hassan", 33);
+        System.out.println(x);
+        System.out.println(m.get("hassann"));
+        System.out.println(p.getFirst());
+    }
+}
